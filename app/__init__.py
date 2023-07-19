09880250348be74f555bbdc9c9778ad6abe97730
Abinash Bordoloi <75088946+Abinash-bordoloi07@users.noreply.github.com>
2023-07-19 17:47:57 +0530

    Features: Log in screen for both user and admin, and database connection

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        load_dotenv(),
+        app.config.setdefault('SECRET_KEY', 'dev'),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)  
+    # ensure the instance folder exists  
+    try:  
+        os.makedirs(app.instance_path)  
+    except OSError:
+        pass
+
+    from routes import user_routes
+    app.register_blueprint(user_routes.user_bp)
+    
+   
+
+    return app
---
+++ app/routes/user_routes.py
@@ -0,0 +1,7 @@
+from flask import ( render_template , Blueprint )
+user_bp = Blueprint('user_bp', __name__)
+@user_bp.route('/user')
+
+@user_bp.route('/user_login')
+def user_login():
+    return render_template('user/userLogin.html')
